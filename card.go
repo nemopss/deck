9c6cf52184a2d4631094e3b58d2beaf2f41efc5d
nemopss <alexey_gladilin@mail.ru>
2024-07-02 20:18:44 +0300

    Add shuffle func

--- card.go
+++ card.go
@@ -4,7 +4,9 @@ package deck
 
 import (
 	"fmt"
+	"math/rand"
 	"sort"
+	"time"
 )
 
 type Suit uint8
@@ -88,4 +90,14 @@ func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
 		sort.Slice(cards, less(cards))
 		return cards
 	}
+}
+
+func Shuffle(cards []Card) []Card {
+	ret := make([]Card, len(cards))
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	perm := r.Perm(len(cards))
+	for i, j := range perm {
+		ret[i] = cards[j]
+	}
+	return ret
 }
